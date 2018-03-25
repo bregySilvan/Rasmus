bf0a913bb6dd8c14e2e1a4fb7a2726bb325cb61d
smergol1 <silvan.bregy@hazu.io>
2018-03-25 15:27:32 +0200

    #add basic css
    -Add some css for the AdvertisementComponent and ListCOmponent.

--- client/src/app/dummy-store.ts
+++ client/src/app/dummy-store.ts
@@ -5,13 +5,13 @@ export const advertisementList: IAdvertisement[] =
 [{
   key: '1',
   type: ElementTypes.advertisement,
-  title: 'Title number 1',
+  name: 'preferred banner',
   description: 'some ad description this is',
   imageURL: 'https://i.imgur.com/IUxU35q.jpg'
 }, {
   key: '2',
   type: ElementTypes.advertisement,
-  title: 'i am the best duimmy title ever made',
+  name: 'To not to use at work',
   description: 'some ad description this is',
 //  imageURL: 'https://ae01.alicdn.com/kf/HTB1KMrVQpXXXXX3XpXXq6xXFXXXw/' +
   //          'Sexy-Mousse-new-Lace-Bra-Set-Floral-thin-Cup-Bras-Sexy-Girls-Lingerie-Underwear-Set-Black.jpg_640x640.jpg'
@@ -19,9 +19,21 @@ export const advertisementList: IAdvertisement[] =
 }, {
   key: '3',
   type: ElementTypes.advertisement,
-  title: 'title Number 3',
+  name: 'Finished last wedn.',
   description: 'some ad description this is',
   imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJn1BGUcO9mlYvq_v6PlrLzQc_G1fWtb6Z73pXsM6ubNXNoLZgZQ'
+}, {
+  key: '4',
+  type: ElementTypes.advertisement,
+  name: 'Finished last wedn.',
+  description: 'some ad description this is',
+  imageURL: 'http://hanassets.nd.gov/images/product/test.png'
+}, {
+  key: '5',
+  type: ElementTypes.advertisement,
+  name: 'Finished last wedn.',
+  description: 'some ad description this is',
+  imageURL: 'https://www.centraltest.com/sites/all/files/platforme-evaluation-en-450x290.png'
 }];
 
 export const advertisementKeys: string[] = ['1', '2', '3'];
@@ -33,8 +45,14 @@ export const listElements: IListElement[] = [
   }, {
     key: '2',
     type: ElementTypes.advertisement
-  }, {
+  },  {
     key: '3',
+    type: ElementTypes.advertisement
+  }, {
+    key: '4',
+    type: ElementTypes.advertisement
+  }, {
+    key: '5',
     type: ElementTypes.advertisement
   }
 ];
